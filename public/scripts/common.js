d1df8b19106158e62a462d8b82a6becc3c84b0a8
nsasin <nikitasasin@gmail.com>
2017-08-15 18:31:36 +0300

    Add 'comments' component. Add post request

--- public/scripts/common.js
+++ public/scripts/common.js
@@ -3,7 +3,32 @@ var PostBox = React.createClass({
 	  return {data: []};
 	},
 
-	componentDidMount: function() {
+  handleCommentSubmit: function(comment) {
+  	'use strict';
+
+  	var comments = this.state.data;
+    comment.id = Date.now();
+    var newComments = comments.concat([comment]);
+    this.setState({data: newComments});
+
+		fetch("/posts", 
+		{
+			method: "POST",
+		  body: JSON.stringify(comment),
+		  headers: {
+		    "Content-Type": "application/json"
+		  }
+		})
+		  .then(function(response) {
+		  	return response.json();
+		   })
+		  .then(function(data) {
+		  	console.log(data);
+		    this.setState({data: data});
+		  }.bind(this))    
+  },
+
+  componentDidMount: function() {
    'use strict';
 
 		fetch("/posts")
@@ -16,12 +41,13 @@ var PostBox = React.createClass({
 		  .catch( alert );
   },
 
+
 	render: function() {
 		return (
 			<div className="post-box">
 				<h1>Posts</h1>
 				<PostList data={this.state.data}/>
-				<PostForm />
+				<PostForm onCommentSubmit={this.handleCommentSubmit}/>
 			</div>
 			);
 	}
@@ -32,7 +58,7 @@ var PostList = React.createClass({
 		var data = this.props.data;
 		var PostListTemplate = data.map(function(item) { 
       return (
-        <Post text={item.text} key={item.id} />
+        <Post text={item.text} id={item.id} key={item.id} />
       );
     });
 		return (
@@ -44,28 +70,100 @@ var PostList = React.createClass({
 });
 
 var Post = React.createClass({
+	getInitialState: function() {
+	  return {data: []};
+	},
+
+	componentDidMount: function() {
+   'use strict';
+
+		fetch("/posts/" + this.props.id + "/comments")
+		  .then(function(response) {
+		  	return response.json();
+		   })
+		  .then(function(data) {
+		    this.setState({data: data});
+		  }.bind(this))
+		  .catch( alert );
+  },
+
 	render: function() {
 		return (
 			<div className="post">
 				<p className="post-text">
 					{this.props.text}
 				</p>
+				<CommentList data={this.state.data}/>
 			</div>
 		);
 	}
 });
 
 var PostForm = React.createClass({
+	getInitialState: function() {
+    return {text: ''};
+  },
+
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var text = this.state.text.trim();
+    if (!text) {
+      return;
+    }
+    this.props.onCommentSubmit({text: text});
+    this.setState({text: ''});
+  },
+
 	render: function() {
 		return (
-			<div className="post-form">
-			Hello, world! I am a postForm.
+			<form className="post-form" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Say something..."
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+        <input type="submit" value="Post" />      
+      </form>
+			);
+	}
+});
+
+var CommentList = React.createClass({
+	render: function() {
+		var data = this.props.data;
+		var CommentListTemplate = data.map(function(item) { 
+      return (
+        <Comment name={item.name} body={item.body} key={item.id} />
+      );
+    });
+		return (
+			<div className="comment-list">
+				<h2>Comments</h2>
+				{CommentListTemplate}
 			</div>
 			);
 	}
 });
 
-
+var Comment = React.createClass({
+	render: function() {
+		return (
+			<div className="comment">
+				<p className="comment-name">
+					{this.props.name}
+				</p>
+				<p className="comment-body">
+					{this.props.body}
+				</p>
+			</div>
+		);
+	}
+});
 
 ReactDOM.render(
 	<PostBox />,
